607fd148c232f16e0c8dc06b071520aa9517f054
MonicaGrages <mgrages1@gmail.com>
2017-07-05 10:52:49 -0400

    set up creatures index route and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Creature.create([{name: "Luke", description: "Jedi"},{name: "Darth Vader", description: "Father of Luke"},{name: "Yoda", description: "Little green guy"}])
