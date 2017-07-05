21725b17e366a9d3313d1cc6eba9254d196f17b3
MonicaGrages <mgrages1@gmail.com>
2017-07-05 14:18:35 -0400

    delete and update creature routes complete

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -19,6 +19,24 @@ class CreaturesController < ApplicationController
     @creature = Creature.find(params[:id])
   end
 
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    @creature = Creature.find(params[:id])
+    if @creature.update(creature_params)
+      redirect_to creature_path
+    end
+  end
+
+  def delete
+    @creature = Creature.find(params[:id])
+    if @creature.delete
+      redirect_to creatures_path
+    end
+  end
+
   private
   def creature_params
     params.require(:creature).permit(:name, :description)
